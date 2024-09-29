23d09fee429414e00e5cc02cd2a9dbb66bb83cc2
Mir Abdul Rehman <mirabdulrehman743@gmail.com>
2024-09-29 12:43:29 +0200

    Firs push

--- R/euclidean.R
+++ R/euclidean.R
@@ -9,6 +9,7 @@
 #' euclidean(123612, 13892347912)  # Returns 4
 #' euclidean(100, 1000)            # Returns 100
 #' @export
+
 euclidean <- function(a, b) {
   stopifnot(is.numeric(a), is.numeric(b))
   while (b != 0) {
